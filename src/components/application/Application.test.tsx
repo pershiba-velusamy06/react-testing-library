0f73d42c460eba8ee586542832ef6abf5403ef75
PershibaVelusamy <pershibavelusamy06@gmail.com>
2024-07-11 19:09:41 +0530

    Application component

---
+++ src/components/application/Application.test.tsx
@@ -0,0 +1,76 @@
+import { render, screen } from '@testing-library/react'
+import { Application } from './Application'
+
+
+describe('Application', () => {
+    test('renders correctly', () => {
+        render(<Application />)
+
+        const pageHeading = screen.getByRole('heading', {
+            level: 1,
+        })
+        expect(pageHeading).toBeInTheDocument()
+
+        const sectionHeading = screen.getByRole('heading', {
+            level: 2,
+        })
+        expect(sectionHeading).toBeInTheDocument()
+
+
+        const nameElement = screen.getByRole('textbox', {
+            name: 'Name',
+        })
+        expect(nameElement).toBeInTheDocument();
+
+
+
+        const bioElement = screen.getByRole('textbox', {
+            name: 'Bio',
+        })
+        expect(bioElement).toBeInTheDocument()
+
+        const jobLocationElement = screen.getByRole('combobox')
+        expect(jobLocationElement).toBeInTheDocument()
+
+
+        const termsElement = screen.getByRole('checkbox')
+        expect(termsElement).toBeInTheDocument()
+
+        const submitElement = screen.getByRole('button')
+        expect(submitElement).toBeInTheDocument()
+        expect(submitElement).toBeDisabled()
+
+        const nameElement2 = screen.getByLabelText('Name', { selector: 'input' })
+        expect(nameElement2).toBeInTheDocument()
+
+
+        const termsElement2 = screen.getByLabelText(
+            'I agree to the terms and conditions'
+        )
+        expect(termsElement2).toBeInTheDocument()
+
+        const nameElement3 = screen.getByPlaceholderText('Fullname')
+        expect(nameElement3).toBeInTheDocument()
+
+
+        const paragraphElement = screen.getByText('All fields are mandatory')
+        expect(paragraphElement).toBeInTheDocument()
+
+
+        const nameElement4 = screen.getByDisplayValue('Pershiba')
+        expect(nameElement4).toBeInTheDocument()
+
+        const imageElement = screen.getByAltText('a person with a laptop')
+        expect(imageElement).toBeInTheDocument()
+
+
+        const closeElement = screen.getByTitle('close')
+        expect(closeElement).toBeInTheDocument()
+
+        
+    const customElement = screen.getByTestId('custom-element')
+    expect(customElement).toBeInTheDocument()
+    
+
+    })
+})
