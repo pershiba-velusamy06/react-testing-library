44f402b83fc9c2fa8fb98eef6eafadd5cea701cd
PershibaVelusamy <pershibavelusamy06@gmail.com>
2024-07-12 20:45:30 +0530

    provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
 import { Application } from './components/application/Application';
+import Skills from './components/skills/Skills';
+import { Counter } from './components/counter/Counter';
 
 function App() {
   return (
     <div className="App">
-   <Application/>
+   {/* <Application/> */}
+   <Skills skills={['HTML', 'CSS', 'JavaScript']}/>
+   <Counter/>
     </div>
   );
 }
---
+++ src/components/mui/MuiMode.test.tsx
@@ -0,0 +1,16 @@
+import { render, screen } from '@testing-library/react'
+import { MuiMode } from './MuiMode'
+
+describe('MuiMode', () => {
+  test('renders text correctly', () => {
+    render(<MuiMode />)
+    const headingElement = screen.getByRole('heading')
+    expect(headingElement).toHaveTextContent('dark mode')
+  })
+
+  test('renders text in white color for dark mode', () => {
+    render(<MuiMode />)
+    const headingElement = screen.getByRole('heading')
+    expect(headingElement).toHaveStyle({ color: "'rgb(255, 255, 255)'" })
+  })
+})
